4885ec15867d1c663e7ae7fbf586654b44c0cb47
Elden Williams <eldenw28@yahoo.com>
2022-04-18 10:28:09 -0700

    Added a form.

---
+++ src/form.js
@@ -0,0 +1,6 @@
+<form name="get a name" method="POST" data-netlify="true">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+</form>
