ea687201873bf1d1872962efed4a52fe968ecf9b
Elden Williams <eldenw28@yahoo.com>
2022-04-18 10:43:07 -0700

    Added a form.

--- src/App.js
+++ src/App.js
@@ -19,6 +19,26 @@ function App() {
         </a>
         <h1>Hello, Pull Request!</h1>
         <h1>Deploy Preview Test</h1>
+        <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
       </header>
     </div>
   );
